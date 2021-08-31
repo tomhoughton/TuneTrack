c81012b9d13536a6ab56e3370533d29a2aed88c4
Thomas Houghton <tomhoughton888@gmail.com>
2021-08-31 22:43:13 +0100

    Added basic User Sign Up
    it just creates a user with the hard coded email and password, next need to implement including the email and password that the user gives

--- WebApp/tune-track-web/src/Views/Login.js
+++ WebApp/tune-track-web/src/Views/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Bootstrap components:
 import Container from 'react-bootstrap/Container';
@@ -8,44 +8,68 @@ import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const Login = () => {
-	return (
-		<Container className='my-auto'>
-			<Row>
-				<Col></Col>
-				<Col>
-					<h1>Sign in</h1>
-				</Col>
-				<Col></Col>
-			</Row>
-			<Row>
-				<Col></Col>
-				<Col>
-					<Card>
-						<Form>
-							<Form.Group className='mb-3' controlId='formBasicEmail'>
-								<Form.Label>Email Address</Form.Label>
-								<Form.Control type='email' placeholder='Enter email'/>
-							</Form.Group>
-							<Form.Group className='mb-3' controlId='formBasicPassword'>
-								<Form.Label>Password</Form.Label>
-								<Form.Control type='password' placeholder='Password'/>
-							</Form.Group>
-						</Form>
-						<Row>
-							<Col>
-								<Button variant='primary'>Sign In</Button>
-							</Col>
-							<Col>
-								<Button variant='secondary'>Sign Up</Button>
-							</Col>
-						</Row>
-					</Card>
-				</Col>
-				<Col></Col>
-			</Row>
-		</Container>
-	);
+// Firebase components:
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+class Login extends Component {
+
+	constructor(props) {
+		super (props);
+
+		this.handleSignUpClick = this.handleSignUpClick.bind(this);
+
+	}
+
+	handleSignUpClick() {
+		const auth = getAuth();
+		createUserWithEmailAndPassword(auth, 'tomhoughton888@gmail.com', 'password').then((userCredential) => {
+			const user = userCredential.user;
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		})
+	}
+
+	render () {
+		return (
+			<Container className='my-auto'>
+				<Row>
+					<Col></Col>
+					<Col>
+						<h1>Sign in</h1>
+					</Col>
+					<Col></Col>
+				</Row>
+				<Row>
+					<Col></Col>
+					<Col>
+						<Card>
+							<Form>
+								<Form.Group className='mb-3' controlId='formBasicEmail'>
+									<Form.Label>Email Address</Form.Label>
+									<Form.Control type='email' placeholder='Enter email'/>
+								</Form.Group>
+								<Form.Group className='mb-3' controlId='formBasicPassword'>
+									<Form.Label>Password</Form.Label>
+									<Form.Control type='password' placeholder='Password'/>
+								</Form.Group>
+							</Form>
+							<Row>
+								<Col>
+									<Button variant='primary'>Sign In</Button>
+								</Col>
+								<Col>
+									<Button variant='secondary' onClick={this.handleSignUpClick}>Sign Up</Button>
+								</Col>
+							</Row>
+						</Card>
+					</Col>
+					<Col></Col>
+				</Row>
+			</Container>
+		);
+	}
 }
 
 export default Login;

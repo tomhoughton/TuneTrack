dc03d765126de78131cae1c43df23bd0839ed448
Thomas Houghton <tomhoughton888@gmail.com>
2021-09-01 20:16:30 +0100

    Login and Sign Up working

--- WebApp/tune-track-web/src/Views/Login.js
+++ WebApp/tune-track-web/src/Views/Login.js
@@ -9,21 +9,40 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 // Firebase components:
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 class Login extends Component {
 
 	constructor(props) {
 		super (props);
 
-		this.handleSignUpClick = this.handleSignUpClick.bind(this);
+		this.state = {
+			email: '',
+			password: ''
+		}
 
+		this.handleSignUpClick = this.handleSignUpClick.bind(this);
+		this.handleSignInClick = this.handleSignInClick.bind(this);
+
+		this.auth = getAuth();
 	}
 
 	handleSignUpClick() {
-		const auth = getAuth();
-		createUserWithEmailAndPassword(auth, 'tomhoughton888@gmail.com', 'password').then((userCredential) => {
+		
+		createUserWithEmailAndPassword(this.auth, this.state.email, this.state.password).then((userCredential) => {
 			const user = userCredential.user;
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		})
+	}
+
+	handleSignInClick() {
+		console.log('sign in')
+		signInWithEmailAndPassword(this.auth, this.state.email, this.state.password).then((userCredential) => {
+			const user = userCredential.user;
+			console.log(user.email);
 		})
 		.catch((error) => {
 			const errorCode = error.code;
@@ -48,16 +67,26 @@ class Login extends Component {
 							<Form>
 								<Form.Group className='mb-3' controlId='formBasicEmail'>
 									<Form.Label>Email Address</Form.Label>
-									<Form.Control type='email' placeholder='Enter email'/>
+									<Form.Control 
+										type='email' 
+										placeholder='Enter email'
+										value={this.state.email}
+										onChange={e => this.setState({ email: e.target.value })}
+										/>
 								</Form.Group>
 								<Form.Group className='mb-3' controlId='formBasicPassword'>
 									<Form.Label>Password</Form.Label>
-									<Form.Control type='password' placeholder='Password'/>
+									<Form.Control 
+									type='password' 
+									placeholder='Password'
+									value={this.state.password}
+									onChange={e => this.setState({ password: e.target.value})}
+								/>
 								</Form.Group>
 							</Form>
 							<Row>
 								<Col>
-									<Button variant='primary'>Sign In</Button>
+									<Button variant='primary' onClick={this.handleSignInClick}>Sign In</Button>
 								</Col>
 								<Col>
 									<Button variant='secondary' onClick={this.handleSignUpClick}>Sign Up</Button>

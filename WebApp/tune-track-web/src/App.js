507bd29ec0cf1a577fde937eb7ba6ae038a4c728
Thomas Houghton <tomhoughton888@gmail.com>
2021-09-01 20:25:01 +0100

    Changed App js to show nav bar and present login screen

--- WebApp/tune-track-web/src/App.js
+++ WebApp/tune-track-web/src/App.js
@@ -1,17 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React, { Component } from 'react';
 // Import my component.
 import Login from './Views/Login';
 
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Button from 'react-bootstrap/Button';
 
-function App() {
+class  App extends Component {
 
-  return (
-    <div className="App">
-      <Login></Login>
-    </div>
-  );
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      view: 'login'
+    }
+
+  }
+
+  render() {
+
+    document.title = 'Tune Track'
+
+    let ui;
+
+    if (this.state.view ==='login') {
+      ui = <Login />
+    }
+
+
+    return (
+      <>
+        <Navbar style={{ backgroundColor: '#30404D' }} className='text-white'>
+            <Navbar.Brand style={{ color: '#ffffff' }}>Tune Track</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Button className="navButton" style={{ paddingRight: '5px'}} variant="primary" onClick={this.handleHomeClick}>Home</Button>
+              <Button className="navButton" style={{ paddingRight: '5px' }} variant="primary" onClick={this.handleCompareClick}>Upload</Button>
+              <Button className="navButton" style={{ paddingRight: '5px' }} variant="info" onClick={this.handleAboutClick}>Account</Button>
+              <Button className="navButton" style={{ paddingRight: '5px' }} variant="info" onClick={this.handleSetHomeClick}>About</Button>
+            </Nav>
+          </Navbar>
+          {
+            ui
+          }
+      </>
+    );
+  }
+
+  
 }
 
 export default App;
